9d26fb518d54972bd0c8961cad6196ec5cfbb821
ivazquez2007 <ivazquez2007@gmail.com>
2025-12-14 20:32:54 +0100

    Eliminado react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Grid, ContactShadows, Text, Line, TransformControls, Environment } from '@react-three/drei';
 import * as THREE from 'three';
